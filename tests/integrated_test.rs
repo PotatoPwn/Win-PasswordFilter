f5965076d045f7e82eaea3f056049682df7b4055
f3rn0s <ffc@null.net>
2025-11-14 15:03:39 +1000

    Fix UNICODE_STRING length issue, ensure lifetime garauntee in testcase, add testcases

--- tests/integrated_test.rs
+++ tests/integrated_test.rs
@@ -2,48 +2,94 @@ use fake::{
     faker::internet::en::{Password, Username},
     Fake,
 };
-use rand::seq::IndexedRandom;
+use lsass_pass_filter::password_filter::PasswordFilter;
 use windows_sys::core::PWSTR;
 use windows_sys::Win32::Foundation::UNICODE_STRING;
-use Passfilt::password_filter::PasswordFilter;
 
-fn string_to_unicode(string_value: String) -> UNICODE_STRING {
-    let buffer: Vec<u16> = string_value.encode_utf16().collect();
+struct UnicodeString {
+    inner: UNICODE_STRING,
+    _buffer: Vec<u16>,
+}
 
-    UNICODE_STRING {
-        Buffer: buffer.as_ptr() as PWSTR,
-        Length: string_value.len() as u16,
-        MaximumLength: string_value.len() as u16,
+fn string_to_unicode(string_value: &str) -> UnicodeString {
+    let mut buffer: Vec<u16> = string_value.encode_utf16().collect();
+    buffer.push(0);
+
+    UnicodeString {
+        inner: UNICODE_STRING {
+            Buffer: buffer.as_ptr() as PWSTR,
+            Length: (string_value.len() * 2) as u16,
+            MaximumLength: ((string_value.len() + 1) * 2) as u16,
+        },
+        _buffer: buffer,
     }
+}
+
+#[test]
+fn valid_secure_password() {
+    let username = "lie2aePh3ahl";
+    let full_name = "Name";
+    let password = "aejuiPheeBafaM8Seikae9eipah7ped2";
+
+    assert!(PasswordFilter(
+        string_to_unicode(&username).inner,
+        string_to_unicode(&full_name).inner,
+        string_to_unicode(&password).inner,
+        true,
+    ));
+}
+
+#[test]
+fn name_in_password_rejected() {
+    let username = "username";
+    let full_name = "Bobby";
+    let password = "Bobby123!";
+
+    // Assert that this will fail
+    assert!(!PasswordFilter(
+        string_to_unicode(&username).inner,
+        string_to_unicode(&full_name).inner,
+        string_to_unicode(&password).inner,
+        true,
+    ));
+}
+
+#[test]
+fn username_in_password_rejected() {
+    let username = "username";
+    let full_name = "Bobby";
+    let password = "username123!";
+
+    // Assert that this will fail
+    assert!(!PasswordFilter(
+        string_to_unicode(&username).inner,
+        string_to_unicode(&full_name).inner,
+        string_to_unicode(&password).inner,
+        true,
+    ));
 }
 
 #[test]
 fn test_run() {
     for _n in 1..100 {
-        let username = Username().fake();
+        let username: String = Username().fake::<String>();
 
-        let full_name = fake::faker::name::en::FirstName().fake();
+        let full_name: String = fake::faker::name::en::FirstName().fake();
 
         let mut rng = rand::rng();
 
-        let password = Password(4..20).fake_with_rng(&mut rng);
+        let password: String = Password(32..50).fake_with_rng(&mut rng);
 
         println!(
             "Username > {}, Password > {}, Full name > {}",
             username, password, full_name
         );
 
-        if PasswordFilter(
-            string_to_unicode(username),
-            string_to_unicode(full_name),
-            string_to_unicode(password),
+        assert!(PasswordFilter(
+            string_to_unicode(&username).inner,
+            string_to_unicode(&full_name).inner,
+            string_to_unicode(&password).inner,
             true,
-        ) {
-            println!("Passed!")
-        } else {
-            eprintln!("Failed!")
-        }
-
-        println!()
+        ));
     }
 }
